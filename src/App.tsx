152b11df3eed967b421ee7d0a6a188e62dc22927
lucas <l.dallier@xsensors.ai>
2020-09-30 18:21:02 -0300

    Config project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+// import { Container } from './styles';
+
+const App: React.FC = () => (
+  <View>
+    <Text>App</Text>
+  </View>
+);
+
+export default App;
