20668a8d1cbf899706bef93c23b9953040a6d53b
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-11-10 19:21:26 -0300

    Change style menu bottom

---
+++ src/components/global/TabBar/styles.ts
@@ -0,0 +1,43 @@
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import styled from 'styled-components/native';
+import colors from '../../../styles/colors';
+
+interface MenuOptionProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View`
+  flex-direction: row;
+  height: 8%;
+  background-color: ${colors.white};
+`;
+
+export const MenuOption = styled.TouchableOpacity<MenuOptionProps>`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const IconMenuOption = styled(FontAwesomeIcon)<MenuOptionProps>`
+  color: ${(props) => (props.isFocused ? colors.primary : colors.black)};
+`;
+
+export const TextMenuOption = styled.Text<MenuOptionProps>`
+  color: ${(props) => (props.isFocused ? colors.primary : colors.black)};
+`;
+
+export const Badge = styled.Text<MenuOptionProps>`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  border-radius: 18;
+  border: 1px solid ${colors.white};
+  top: 5px;
+  right: 24px;
+
+  color: ${colors.white};
+  background-color: ${(props) =>
+    props.isFocused ? colors.black : colors.primary};
+`;
